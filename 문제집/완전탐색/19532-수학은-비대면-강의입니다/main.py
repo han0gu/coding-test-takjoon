dbc0a9a05a11ce7e604b4d391aa4039bd00be885
han0gu <imgum9@gmail.com>
2026-05-21 17:32:25 +0900

    docs(19532): apply Codex feedback

--- 문제집/완전탐색/19532-수학은-비대면-강의입니다/main.py
+++ 문제집/완전탐색/19532-수학은-비대면-강의입니다/main.py
@@ -17,7 +17,7 @@ a,b,c,d,e,f = map(int, input().split())
 flag = False
 for x in range(-999, 1000):
     for y in range(-999, 1000):
-        if ((a + d) * x + (b + e) * y) == c + f:
+        if a*x + b*y == c and d*x + e*y == f:
             flag = True
             print(f"{x} {y}")
         
