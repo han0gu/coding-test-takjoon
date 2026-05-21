c798c8b502781663565bb49b7e5f3f5109b4d3ac
han0gu <imgum9@gmail.com>
2026-05-22 00:07:47 +0900

    docs(17252): apply Codex feedback

--- 문제집/완전탐색/17252-삼삼한-수/main.py
+++ 문제집/완전탐색/17252-삼삼한-수/main.py
@@ -1,5 +1,5 @@
 """
-1 <= N <= 2,147,483,647
+0 <= N <= 2,147,483,647
 각 수를 한번씩만 더해야 함
 """
 """
@@ -10,32 +10,24 @@ EOF
 python3 '문제집/완전탐색/17252-삼삼한-수/main.py' << 'EOF'
 298
 EOF
+
+python3 '문제집/완전탐색/17252-삼삼한-수/main.py' << 'EOF'
+0
+EOF
+
+python3 '문제집/완전탐색/17252-삼삼한-수/main.py' << 'EOF'
+1
+EOF
 """
 
 n = int(input())
 
-answer_count = 0
-
-def check(cur_pow, is_added, accumulated_sum):
-    cur_num = 3 ** cur_pow
-    
-    if cur_num > n:
-        return
-    
-    if is_added:
-        accumulated_sum += cur_num
-
-        if accumulated_sum < n:
-            check(cur_pow + 1, True, accumulated_sum)
-            check(cur_pow + 1, False, accumulated_sum)
-        if accumulated_sum == n:
-            global answer_count
-            answer_count += 1
-    else:
-        check(cur_pow + 1, True, accumulated_sum)
-        check(cur_pow + 1, False, accumulated_sum)
-
-check(0, True, 0)
-check(0, False, 0)
-
-print('YES' if answer_count > 0 else 'NO')
+if n == 0:
+    print("NO")
+else:
+    ternary_digits = ''
+    while n > 0:
+        ternary_digits += str(n % 3)
+        n //= 3
+    # print("ternary_digits", ternary_digits)
+    print('NO' if ternary_digits.find('2') > -1 else 'YES')
