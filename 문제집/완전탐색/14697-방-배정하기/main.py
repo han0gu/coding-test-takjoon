2ea0dd3dbe61d69a8cbfc17621fb7449ad509156
han0gu <imgum9@gmail.com>
2026-05-21 13:03:13 +0900

    docs(14697): try 1

---
+++ 문제집/완전탐색/14697-방-배정하기/main.py
@@ -0,0 +1,28 @@
+"""
+모든 방에 빈 침대가 없어야 함
+모든 종류의 방을 사용하지 않아도 됨, 일부만 사용해도 됨
+각 방의 정원 1 ~ 50명
+전체 학생 수 1 ~ 300명
+"""
+
+a, b, c, n = map(int, input().split())
+
+flag = False
+for i in range(n // a + 1):
+    if flag:
+        break
+    
+    for j in range(n // b + 1):
+        if flag:
+            break
+
+        for k in range(n//c + 1):
+            if flag:
+                break
+
+            flag = a*i + b*j + c*k == n
+
+if flag:
+    print(1)
+else:
+    print(0)
