861dd3c59f3687977b3d3b949cda3293417fba58
han0gu <imgum9@gmail.com>
2026-05-23 18:52:01 +0900

    docs(max-unique-substring-length-in-a-session): try 1

---
+++ hacker-rank/software-engineer-prep-kit/practice-challenges/strings-and-pattern-matching/max-unique-substring-length-in-a-session/main.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+#
+# Complete the 'maxDistinctSubstringLengthInSessions' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING sessionString as parameter.
+#
+
+def maxDistinctSubstringLengthInSessions(sessionString):
+    if not sessionString:
+        return 0
+        
+    splitted = sessionString.split('*')
+    # print('splitted', splitted)
+    if not splitted:
+        return 0
+    
+    answer = 0
+    for string in splitted:
+        visited = []
+        
+        for c in string:
+            if c in visited:
+                answer = max(answer, len(visited))
+                break
+            visited.append(c)
+        
+        # print('visited', visited, answer)
+        
+    return answer
+    
+
+if __name__ == '__main__':
+    sessionString = input()
+
+    result = maxDistinctSubstringLengthInSessions(sessionString)
+
+    print(result)
