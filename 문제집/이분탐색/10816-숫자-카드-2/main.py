21845d56166e6a2b497f8022fba0e443412968ee
han0gu <imgum9@gmail.com>
2026-06-08 23:46:11 +0900

    docs(10816): Codex feedback 반영

--- 문제집/이분탐색/10816-숫자-카드-2/main.py
+++ 문제집/이분탐색/10816-숫자-카드-2/main.py
@@ -4,15 +4,15 @@ n = int(input())
 n_list = list(map(int, input().split()))
 # print('have_list', have_list)
 
-target = int(input())
+m = int(input())
 m_list = list(map(int, input().split()))
 # print('target_list', target_list)
 
 counter = Counter(n_list)
 # print('counter', counter)
 
-answer = ''
-for i, m in enumerate(m_list):
-    answer += str(counter[m]) if counter[m] else '0'
+answer = [None] * m
+for i, target in enumerate(m_list):
+    answer[i] = str(counter[target])
 
 print(" ".join(answer))
