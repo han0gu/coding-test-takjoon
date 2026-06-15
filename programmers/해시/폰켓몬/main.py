67e7aedca0819242757cc68501453d2faa735f78
han0gu <imgum9@gmail.com>
2026-06-15 12:47:57 +0900

    docs(programmers): 폰켓몬 / hash로 풀이

--- programmers/해시/폰켓몬/main.py
+++ programmers/해시/폰켓몬/main.py
@@ -1,19 +1,8 @@
-from itertools import combinations
+from collections import Counter
 
 def solution(nums):
-    comb = combinations(nums, len(nums) // 2)
-    print('comb', comb)
+    c = Counter(nums)
+    keys = c.keys()
+    len_keys = len(keys)
     
-    # for c in comb:
-    #     print(c)
-        
-    set_1 = set(comb)
-    # print('set_1', set_1)
-    
-    set_2 = [set(s) for s in set_1]
-    # print('set_2', set_2)
-    
-    set_3 = sorted(set_2, key=lambda x: -len(x))
-    # print('set_3', set_3)
-    
-    return len(set_3[0])
+    return len_keys if len_keys <= len(nums) // 2 else len(nums) // 2
