72a2cc0d69e12f851e3313886354be3f51f96fb6
han0gu <imgum9@gmail.com>
2026-06-15 22:13:46 +0900

    docs(programmers): 의상 / Counter로 풀이 / `시간 초과`

--- programmers/해시/의상/main.py
+++ programmers/해시/의상/main.py
@@ -6,32 +6,27 @@
 같은 이름의 의상 없음
 전체 의상 1~30개
 """
-from collections import defaultdict
+from collections import Counter
 from itertools import combinations
+import math
 
 def solution(clothes):
     answer = 0
     
     # 카테고리별로 dict 생성
-    category_dict = defaultdict(list)
-    for clth, cate in clothes:
-        category_dict[cate].append(clth)
+    category_dict = Counter([c[1] for c in clothes])
     # print('category_dict', category_dict)
     
     
     # 1,2,3, ... , n개의 카테고리를 선택하는 모든 경우의 수(tuple)를 구함
     keys = category_dict.keys()
-    all_combs = []
     for i in range(len(keys)):
-        all_combs.extend(combinations(keys, i+1))
-    # print('all_combs', all_combs)
-    
-    
-    # 각 카테고리에 포함된 의상의 개수를 곱셈
-    for comb in all_combs:
-        cnt = 1
-        for c in comb:
-            cnt *= len(category_dict[c])
-        answer += cnt
-    
+        for comb in combinations(keys, i+1):
+            # print('comb', comb)
+            
+            cnt = math.prod([category_dict[x] for x in comb])
+            # print('cnt', cnt)
+            
+            answer += cnt
+
     return answer
