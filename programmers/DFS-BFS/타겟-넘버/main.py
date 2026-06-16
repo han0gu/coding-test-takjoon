92c5c47385aaa0fc98f90236c72336fed4aa516f
han0gu <imgum9@gmail.com>
2026-06-16 23:55:03 +0900

    docs(programmers): 타겟 넘버 / DFS로 풀이

--- programmers/DFS-BFS/타겟-넘버/main.py
+++ programmers/DFS-BFS/타겟-넘버/main.py
@@ -1,21 +1,14 @@
-"""
-각 자리마다 +- 두 가지 경우 가능 
--> 모든 조합의 경우의 수를 이진수로 구함
--> 계산 후 target과 일치하는지 판단
-"""
 def solution(numbers, target):
     answer = 0
-    n = len(numbers)
+    stack = [(0, numbers[0]), (0, -numbers[0])] # (index, acc)
     
-    for i in range(2**n):
-        binary = bin(i)[2:].zfill(n)
-        tmp = 0
-
-        for ch, n in zip(binary, numbers):
-            tmp += n if ch == '1' else -1 * n
-        if tmp == target:
+    while stack:
+        i, acc = stack.pop()
+        
+        if i < len(numbers) - 1:
+            stack.append( (i+1, acc + numbers[i+1]) )
+            stack.append( (i+1, acc - numbers[i+1]) )
+        elif acc == target:
             answer += 1
-                
+        
     return answer
-    
-    
