58a82189dbd292ca11fd80ec940920e28044bc4b
han0gu <imgum9@gmail.com>
2026-06-16 17:41:53 +0900

    docs(programmers): 다리를 지나는 트럭 / Codex feedback을 반영하여 queue 로직 개선

--- programmers/스택-큐/다리를-지나는-트럭/main.py
+++ programmers/스택-큐/다리를-지나는-트럭/main.py
@@ -2,32 +2,26 @@ from collections import deque
 
 def solution(bridge_length, weight_limit, truck_weights):
     answer = 0 # 최소 몇 초 걸리냐
-    
-    remain_trucks = deque(truck_weights)
-    bridge_q = deque()
-    # print('remain_trucks', remain_trucks)
-    # print('bridge_q', bridge_q)
-    
-    # 모든 트럭이 건널 때까지
-    while remain_trucks or bridge_q:
-        # print('remain_trucks', remain_trucks)
-        # print('bridge_q', bridge_q)
-        
+
+    bridge = deque([0] * bridge_length)
+    trucks = deque(truck_weights)
+    cur_bridge_weight = 0
+
+    while trucks or cur_bridge_weight > 0:
         answer += 1
-        
-        # 다리 위의 모든 트럭 진행도 +1
-        for i, truck in enumerate(bridge_q):
-            weight, process = truck
-            bridge_q[i] = (weight, process + 1)
-            
-        # 다리 위 첫번째 트럭의 완료 여부 판단
-        if bridge_q and bridge_q[0][1] > bridge_length:
-            bridge_q.popleft()
-        
-        # 남은 트럭 중 첫번째 트럭이 다리에 올라갈 수 있는지 판단
-        if remain_trucks and sum([w for w, _ in bridge_q]) + remain_trucks[0] <= weight_limit:
-            bridge_q.append( (remain_trucks[0], 1) )
-            remain_trucks.popleft()
+
+        # 맨 왼쪽 트럭 처리
+        cur_bridge_weight -= bridge.popleft()
+
+        # 새로운 트럭 추가 시도
+        if trucks:
+            if cur_bridge_weight + trucks[0] <= weight_limit:
+                cur_truck = trucks.popleft()
+                bridge.append(cur_truck)
+                cur_bridge_weight += cur_truck
+            else:
+                bridge.append(0)
+        else:
+            bridge.append(0)
 
     return answer
-    
