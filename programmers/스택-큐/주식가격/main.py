f771958cebdbc11c6898cf6471a2aa2371cbaf52
han0gu <imgum9@gmail.com>
2026-06-16 20:50:51 +0900

    docs(programmers): 주식가격 / Codex feedback을 반영하여 stack으로 풀이 / `monotonic stack, 단조 스택`

--- programmers/스택-큐/주식가격/main.py
+++ programmers/스택-큐/주식가격/main.py
@@ -1,16 +1,21 @@
+"""
+완전 탐색은 O(n^2) -> 스택/큐/해시 등
+
+가격이 떨어지지 않은 놈들은 나중에 한번에 역산할 수 있다 -> 가격이 떨어지는 순간 즉시 계산/업데이트 한다
+"""
 def solution(prices):
-    answer = []
+    answer = [0] * len(prices)
+    remain_indices = [] # 가격이 떨어지지 않은 놈들
     
-    for i in range(len(prices) - 1):
-        cnt = 0
+    for i, p in enumerate(prices):
+        # 가격이 내려가는 순간 즉시 계산/업데이트
+        while remain_indices and prices[remain_indices[-1]] > p:
+            top = remain_indices.pop()
+            answer[top] = i - top
         
-        for j in range(i+1, len(prices)):
-            cnt += 1
-            
-            if prices[i] > prices[j]:
-                break
+        remain_indices.append(i)
+    
+    for i in remain_indices:
+        answer[i] = len(prices) - 1 - i
         
-        answer.append(cnt)
-        
-    answer.append(0)
     return answer
