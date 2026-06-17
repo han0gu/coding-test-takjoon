f92d061d2d5b799fcd2ba3fcbc7e597519b793ec
han0gu <imgum9@gmail.com>
2026-06-18 08:36:49 +0900

    docs(programmers): 게임 맵 최단거리 / Codex feedback을 반영하여 BFS 로직 개선

--- programmers/DFS-BFS/게임-맵-최단거리/main.py
+++ programmers/DFS-BFS/게임-맵-최단거리/main.py
@@ -1,27 +1,22 @@
 from collections import deque
 
 def solution(maps):
-    answer = -1
-    
     n = len(maps)
     m = len(maps[0])
     
     visited = [[False] * m for _ in range(n)]
     
+    visited[0][0] = True
     q = deque([(0,0,1)])
+    
     directions = [[-1,0], [0,1], [1,0], [0,-1]]
     
     while q:
         r, c, acc = q.popleft()
         
-        if visited[r][c]:
-            continue
-        
         if r == n -1 and c == m - 1:
             return acc
-            
-        visited[r][c] = True
-                
+
         for d_r, d_c in directions:
             new_r = r + d_r
             new_c = c + d_c
@@ -30,7 +25,8 @@ def solution(maps):
                0 <= new_c < m and
                not visited[new_r][new_c]
                and maps[new_r][new_c] == 1):
+                visited[new_r][new_c] = True
                 q.append((new_r, new_c, acc + 1))
                 
-    return answer
+    return -1
         
