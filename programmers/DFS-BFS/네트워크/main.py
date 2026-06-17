64c40ae2f052041ab93a4aebaf92e2bd233d2e86
han0gu <imgum9@gmail.com>
2026-06-17 12:55:06 +0900

    docs(programmers): 네트워크 / Codex feedback 문서 정리

--- programmers/DFS-BFS/네트워크/main.py
+++ programmers/DFS-BFS/네트워크/main.py
@@ -9,24 +9,24 @@ from collections import deque
 
 def solution(n, computers):
     answer = 0
-    
+
     visited = [False] * n
     q = deque()
-    
+
     for i in range(n):
         # 시작점 처리
         if not visited[i]:
             q.append(i)
             visited[i] = True
             answer += 1
-            
+
         # 인접 노드 처리
         while q:
             cur_node = q.popleft()
-            
+
             for j in range(n):
                 if not visited[j] and computers[cur_node][j] == 1:
                     q.append(j)
                     visited[j] = True
-    
+
     return answer
