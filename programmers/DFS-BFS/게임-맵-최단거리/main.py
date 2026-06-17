fbccd6d6357f5b50ca3b0965e199245ea73ce8dd
han0gu <imgum9@gmail.com>
2026-06-18 08:27:36 +0900

    docs(programmers): 게임 맵 최단거리 / BFS로 풀이

--- programmers/DFS-BFS/게임-맵-최단거리/main.py
+++ programmers/DFS-BFS/게임-맵-최단거리/main.py
@@ -1,32 +1,36 @@
+from collections import deque
+
 def solution(maps):
     answer = -1
     
     n = len(maps)
     m = len(maps[0])
     
-    stack = [(0,0,1)]
     visited = [[False] * m for _ in range(n)]
-    directions = [[0,1], [1,0], [0,-1], [-1,0]]
     
-    while stack:
-        row, col, acc = stack.pop()
+    q = deque([(0,0,1)])
+    directions = [[-1,0], [0,1], [1,0], [0,-1]]
+    
+    while q:
+        r, c, acc = q.popleft()
         
-        if visited[row][col]:
+        if visited[r][c]:
             continue
         
-        visited[row][col] = True
-        
-        if row == n - 1 and col == m - 1:
-            answer = acc if answer == -1 else min(answer, acc)
-        
-        for d_r, d_c in directions:
-            new_r = row + d_r
-            new_c = col + d_c
+        if r == n -1 and c == m - 1:
+            return acc
             
-            if (0 <= new_r < n and 
-                0 <= new_c < m and 
-                maps[new_r][new_c] == 1 and 
-                not visited[new_r][new_c]):
-                stack.append((new_r, new_c, acc + 1))
+        visited[r][c] = True
+                
+        for d_r, d_c in directions:
+            new_r = r + d_r
+            new_c = c + d_c
+            
+            if (0 <= new_r < n and
+               0 <= new_c < m and
+               not visited[new_r][new_c]
+               and maps[new_r][new_c] == 1):
+                q.append((new_r, new_c, acc + 1))
                 
     return answer
+        
