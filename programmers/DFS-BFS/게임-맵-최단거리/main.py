886cb1d3353a808215b090be42ae6170be950550
han0gu <imgum9@gmail.com>
2026-06-18 08:49:18 +0900

    docs(programmers): 게임 맵 최단거리 / Gemini Code Assist feedback을 반영하여 성능 및 메모리 개선

--- programmers/DFS-BFS/게임-맵-최단거리/main.py
+++ programmers/DFS-BFS/게임-맵-최단거리/main.py
@@ -9,7 +9,7 @@ def solution(maps):
     visited[0][0] = True
     q = deque([(0, 0, 1)])
 
-    directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+    directions = ((-1,0), (0,1), (1,0), (0,-1))
 
     while q:
         r, c, acc = q.popleft()
@@ -21,10 +21,12 @@ def solution(maps):
             new_r = r + d_r
             new_c = c + d_c
 
-            if (0 <= new_r < n and
-               0 <= new_c < m and
-               not visited[new_r][new_c]
-               and maps[new_r][new_c] == 1):
+            if (
+                0 <= new_r < n and 
+                0 <= new_c < m and 
+                not visited[new_r][new_c] 
+                and maps[new_r][new_c] == 1
+            ):
                 visited[new_r][new_c] = True
                 q.append((new_r, new_c, acc + 1))
 
