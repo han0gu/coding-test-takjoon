9b4bd4f2473c8290c250e8025ca23028e64bb682
han0gu <imgum9@gmail.com>
2026-06-17 12:47:50 +0900

    docs(programmers): 네트워크 / Codex feedback을 반영하여 BFS 로직 개선

--- programmers/DFS-BFS/네트워크/main.py
+++ programmers/DFS-BFS/네트워크/main.py
@@ -1,42 +1,31 @@
 """
 BFS
 
-특정 노드를 pop, 방문 처리, answer += 1
--> 인접 노드를 append
--> queue가 빈 경우 네크워크 하나 끝
+네트워크 시작점을 q에 추가, answer += 1,
+-> 특정 노드를 pop 하면서 인접 노드를 추가
+-> q가 비어 있으면 해당 네트워크 처리 완료
 """
 from collections import deque
 
 def solution(n, computers):
     answer = 0
     
-    row_cnt = len(computers)
-    col_cnt = len(computers[0])
-    
-    visited = [[False] * col_cnt for _ in range(row_cnt)]
-    
+    visited = [False] * n
     q = deque()
     
-    for r in range(row_cnt):
-        for c in range(col_cnt):
-            # 네트워크 시작점 등록
-            if not visited[r][c] and computers[r][c] == 1:
-                q.append((r,c))
-                answer += 1
-                # print('start', r, c)
+    for i in range(n):
+        # 시작점 처리
+        if not visited[i]:
+            q.append(i)
+            answer += 1
             
-            # 인접 노드 처리
-            while q:
-                # 시작점 처리
-                cur_r, cur_c = q.popleft()
-                visited[cur_r][cur_c] = True
-                visited[cur_c][cur_r] = True
-                
-                # 시작점에 연결된 모든 노드 처리
-                for i in range(col_cnt):
-                    if not visited[cur_r][i] and computers[cur_r][i] == 1:
-                        # print('near', cur_r, i)
-                        q.append((cur_r, i))
-                        q.append((i, cur_r))   
+        # 인접 노드 처리
+        while q:
+            cur_node = q.popleft()
+            visited[cur_node] = True
             
+            for j in range(n):
+                if cur_node != j and not visited[j] and computers[cur_node][j] == 1:
+                    q.append(j)
+    
     return answer
