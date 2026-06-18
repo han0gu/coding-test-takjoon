ac32cf00aa7fc257a24ef428fd37f7ce2a550991
han0gu <imgum9@gmail.com>
2026-06-18 17:08:52 +0900

    docs(1182): Codex feedback을 반영하여 백트래킹으로 풀이

--- 문제집/백트래킹/1182-부분수열의-합/main.py
+++ 문제집/백트래킹/1182-부분수열의-합/main.py
@@ -1,13 +1,19 @@
-from itertools import combinations
-
 n, target = map(int, input().split())
 nums = list(map(int, input().split()))
 
 answer = 0
 
-for i in range(1,n+1):
-    for c in combinations(nums, i):
-        if sum(c) == target:
-            answer += 1
+stack = [(0, nums[0]), (0, 0)]
 
-print(answer)
+while stack:
+    idx, value = stack.pop()
+
+    if idx == n-1:
+        if value == target:
+            answer += 1
+        continue
+    
+    stack.append( (idx + 1, value + nums[idx+1]) )
+    stack.append( (idx + 1, value) )
+    
+print(answer if target != 0 else answer - 1)
