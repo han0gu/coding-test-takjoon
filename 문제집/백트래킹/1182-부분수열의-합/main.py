5a9ccb42a821aa01ff68b412287d065c75cdad74
han0gu <imgum9@gmail.com>
2026-06-18 16:33:38 +0900

    docs(1182): 조합으로 풀이

--- 문제집/백트래킹/1182-부분수열의-합/main.py
+++ 문제집/백트래킹/1182-부분수열의-합/main.py
@@ -1,33 +1,13 @@
-"""
-원소의 개수가 1~n개인 모든 부분집합을 구함
+from itertools import combinations
 
-원소의 개수가 k개인 부분집합을 구할 때는 k-1개인 부분집합의 결과를 재활용함
-"""
 n, target = map(int, input().split())
 nums = list(map(int, input().split()))
 
 answer = 0
 
-# 개수가 1개
-candidates = {}
-candidates[1] = [*nums]
-# print('candidates', candidates)
-
-# 개수가 2 ~ n개인 부분집합 계산
-for k in range(1, n): # 1~4
-    reuse = candidates[k]
-    # print('reuse', reuse)
-
-    candidates[k+1] = [] # 2, 3, 4
-    
-    r = range(k, n) # 1~4, 2~4, 3~4, 4
-    for i in r:
-        candidates[k+1].append(reuse[i-k] + nums[i])
-# print('candidates', candidates)
-
-for nums in candidates.values():
-    for n in nums:
-        if n == target:
+for i in range(1,n+1):
+    for c in combinations(nums, i):
+        if sum(c) == target:
             answer += 1
 
 print(answer)
