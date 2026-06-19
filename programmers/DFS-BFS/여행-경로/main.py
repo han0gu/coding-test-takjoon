aef302122ea7d8205ff533f8897af8020080dd5f
han0gu <imgum9@gmail.com>
2026-06-19 20:58:15 +0900

    fix(programmers): 여행 경로 풀이를 반복 스택으로 개선

--- programmers/DFS-BFS/여행-경로/main.py
+++ programmers/DFS-BFS/여행-경로/main.py
@@ -1,26 +1,21 @@
+from collections import defaultdict
+
+
 def solution(tickets):
-    tickets.sort()
-    
-    n = len(tickets)
-    visited = [False] * n
-    route = ["ICN"]
-    
-    def dfs(cur_airport):
-        if len(route) == n + 1:
-            return True
-        
-        for i, (start, end) in enumerate(tickets):
-            if not visited[i] and start == cur_airport:
-                visited[i] = True
-                route.append(end)
-                
-                if dfs(end):
-                    return True
-                
-                route.pop()
-                visited[i] = False
-        
-        return False
-    
-    dfs("ICN")
-    return route
+    graph = defaultdict(list)
+
+    for start, end in sorted(tickets, reverse=True):
+        graph[start].append(end)
+
+    stack = ["ICN"]
+    route = []
+
+    while stack:
+        cur_airport = stack[-1]
+
+        if graph[cur_airport]:
+            stack.append(graph[cur_airport].pop())
+        else:
+            route.append(stack.pop())
+
+    return route[::-1]
