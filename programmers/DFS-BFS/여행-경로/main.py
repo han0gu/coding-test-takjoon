baa321ab4b09eb969986b1220c3ad02736d4fe10
han0gu <imgum9@gmail.com>
2026-06-19 20:35:28 +0900

    docs(programmers): 여행 경로 / Codex feedback을 반영한 DFS 풀이 / `Codex가 작성`

--- programmers/DFS-BFS/여행-경로/main.py
+++ programmers/DFS-BFS/여행-경로/main.py
@@ -1,25 +1,26 @@
 def solution(tickets):
-    answer = []
+    tickets.sort()
     
     n = len(tickets)
-    stack = [('ICN', 0, [False] * n, ['ICN'])] # (도착지, 사용한 티켓 수, 티켓 사용 여부, 정답 리스트)
+    visited = [False] * n
+    route = ["ICN"]
     
-    while stack:
-        cur_e, cnt, visited, cur_answer = stack.pop()
+    def dfs(cur_airport):
+        if len(route) == n + 1:
+            return True
         
-        if cnt == n:
-            answer.append(cur_answer)
-            continue
-            
-        for i, (nxt_s, nxt_e) in enumerate(tickets):
-            if not visited[i] and cur_e == nxt_s:
-                new_visited = [*visited]
-                new_answer = [*cur_answer]
+        for i, (start, end) in enumerate(tickets):
+            if not visited[i] and start == cur_airport:
+                visited[i] = True
+                route.append(end)
                 
-                new_visited[i] = True
-                new_answer.append(nxt_e)
+                if dfs(end):
+                    return True
                 
-                stack.append( (nxt_e, cnt + 1, new_visited, new_answer) )
+                route.pop()
+                visited[i] = False
+        
+        return False
     
-    return min(answer)
-    
+    dfs("ICN")
+    return route
