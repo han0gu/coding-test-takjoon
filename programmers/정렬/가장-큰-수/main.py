43bff4b5f80e511cda8da51c823c65339c76ec12
han0gu <imgum9@gmail.com>
2026-06-19 22:57:59 +0900

    docs(programmers): 가장 큰 수 / best practice / `Codex가 작성`

--- programmers/정렬/가장-큰-수/main.py
+++ programmers/정렬/가장-큰-수/main.py
@@ -1,13 +1,17 @@
 from functools import cmp_to_key
 
-def compare(s1, s2):
-    return int(s1 + s2) - int(s2 + s1)
+
+def compare(a, b):
+    if a + b > b + a:
+        return -1
+    if a + b < b + a:
+        return 1
+    return 0
+
 
 def solution(numbers):
-    ls = [str(x) for x in numbers]
-    # print('ls', ls)
-    
-    ls.sort(key=cmp_to_key(compare), reverse=True)
-    # print('ls', ls)
-    
-    return ''.join(ls) if ls[0] != '0' else '0'
+    nums = [str(number) for number in numbers]
+    nums.sort(key=cmp_to_key(compare))
+
+    answer = ''.join(nums)
+    return '0' if answer[0] == '0' else answer
