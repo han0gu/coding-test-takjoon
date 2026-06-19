412a09fb1aea9494ba5b478d5fe37329de34e410
han0gu <imgum9@gmail.com>
2026-06-19 16:05:35 +0900

    docs(1759): 백트래킹으로 풀이

--- 문제집/백트래킹/1759-암호-만들기/main.py
+++ 문제집/백트래킹/1759-암호-만들기/main.py
@@ -1,5 +1,3 @@
-from itertools import combinations
-
 L, C = map(int, input().split())
 candidates = input().split()
 candidates.sort()
@@ -12,7 +10,24 @@ def is_valid(chars):
             cnt_moum += 1
     return cnt_moum >= 1 and len(chars) - cnt_moum >= 2
 
-answer = []
-for comb in combinations(candidates, L):
-    if is_valid(comb):
-        print(''.join(comb))
+stack = [] # (검토한 마지막 인덱스, 선택한 문자 수, 선택한 문자 리스트)
+stack = [
+    (0, 0, ''),
+    (0, 1, candidates[0])
+]
+
+while stack:
+    cur_idx, selected_cnt, chars = stack.pop()
+
+    if selected_cnt == L:
+        if is_valid(chars):
+            print(chars)
+        continue
+
+    if cur_idx == C - 1:
+        continue
+
+    # 다음 문자 미선택
+    stack.append( (cur_idx + 1, selected_cnt, chars) )
+    # 다음 문자 선택
+    stack.append( (cur_idx + 1, selected_cnt + 1, chars + candidates[cur_idx + 1]) )
