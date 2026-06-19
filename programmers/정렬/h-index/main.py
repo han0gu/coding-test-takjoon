288fa93562c3c99c36256cb83326ebd3225fec83
han0gu <imgum9@gmail.com>
2026-06-19 23:18:27 +0900

    style(programmers): h-index 공백 정리 / `Codex가 작성`

--- programmers/정렬/h-index/main.py
+++ programmers/정렬/h-index/main.py
@@ -1,9 +1,9 @@
 def solution(citations):
     n = len(citations)
     citations.sort(reverse=True)
-    
+
     for i in range(n, 0, -1):
         if citations[i-1] >= i:
             return i
-    
+
     return 0
