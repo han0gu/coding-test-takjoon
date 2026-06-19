44dd6404e245f3760661d624d0f47e33dc0a1de0
han0gu <imgum9@gmail.com>
2026-06-19 20:09:11 +0900

    docs(programmers): 여행 경로 / Codex feedback을 반영하여 BFS로 풀이 개선

--- programmers/DFS-BFS/여행-경로/main.py
+++ programmers/DFS-BFS/여행-경로/main.py
@@ -3,28 +3,24 @@ from collections import deque
 def solution(tickets):
     answer = []
     n = len(tickets)
-    
-    q = deque([('ICN', 0, [], ['ICN'])]) # (도착지, 검토할 티켓 인덱스, 이미 검토한 티켓 인덱스 리스트, answer)
+
+    q = deque([('ICN', 0, [False] * n, ['ICN'])]) # (도착지, 사용한 티켓 개수, 티켓 사용 여부 리스트, answer)
     
     while q:
-        cur_e, idx, visited_idx, cur_answer = q.popleft()
+        cur_e, used_cnt, visited, cur_answer = q.popleft()
         
-        if idx == n:
+        if used_cnt == n:
             answer.append(cur_answer)
+            continue
         
         for i, (nxt_s, nxt_e) in enumerate(tickets):
-            if i not in visited_idx and cur_e == nxt_s:
-                new_visited_idx = [*visited_idx]
+            if not visited[i] and cur_e == nxt_s:
+                new_visited = [*visited]
                 new_answer = [*cur_answer]
                 
-                new_visited_idx.append(i)
+                new_visited[i] = True
                 new_answer.append(nxt_e)
                 
-                q.append( (nxt_e, idx + 1, new_visited_idx, new_answer) )
-                
+                q.append( (nxt_e, used_cnt + 1, new_visited, new_answer) )
     
-    if len(answer) == 1:
-        return answer[0]
-    else:
-        answer.sort()
-        return answer[0]
+    return min(answer)
