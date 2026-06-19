f92852e4080726b290e52362bafe3df1433e7c66
han0gu <imgum9@gmail.com>
2026-06-19 18:13:07 +0900

    docs(programmers): 단어 변환 / Codex feedback을 반영하여 성능 개선

--- programmers/DFS-BFS/단어-변환/main.py
+++ programmers/DFS-BFS/단어-변환/main.py
@@ -2,15 +2,21 @@ from collections import deque
 
 def can_change(w1, w2):
     diff_cnt = 0
+    
     for i in range(len(w1)):
         if w1[i] != w2[i]:
             diff_cnt += 1
+            
+            if diff_cnt > 1:
+                return False
+            
     return diff_cnt == 1
 
 def solution(begin, target, words):
-    
-    answer = 0
-    
+
+    if target not in words:
+        return 0
+
     visited = [False] * len(words)
     q = deque([(begin, 0)]) # (현재 단어, 누적 변경 횟수)
     
@@ -18,11 +24,11 @@ def solution(begin, target, words):
         cur, cnt = q.popleft()
         
         if cur == target:
-            answer = min(answer, cnt) if answer != 0 else cnt
+            return cnt
             
         for i, w in enumerate(words):
             if not visited[i] and can_change(cur, w):
                 visited[i] = True
                 q.append((w, cnt + 1))
                 
-    return answer
+    return 0
