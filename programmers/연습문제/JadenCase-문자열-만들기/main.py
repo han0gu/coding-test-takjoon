1fa2d9fa3138e76012ca034893d82180edb78cc0
han0gu <imgum9@gmail.com>
2026-06-20 10:49:56 +0900

    docs(programmers): JadenCase 문자열 만들기 / 공백 정리 / `Codex가 작성`

--- programmers/연습문제/JadenCase-문자열-만들기/main.py
+++ programmers/연습문제/JadenCase-문자열-만들기/main.py
@@ -1,14 +1,13 @@
 def solution(s):
     answer = []
-    
+
     words = s.split(' ')
-    
+
     for w in words:
         if w:
             w = w.lower()
             w = w[0].upper() + w[1:]
-        
+
         answer.append(w)
-        
+
     return ' '.join(answer)
-    
