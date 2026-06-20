098e798a098568b4379344a0529b0146ae77c106
han0gu <imgum9@gmail.com>
2026-06-20 12:24:49 +0900

    docs(programmers): 숫자의 표현 Gemini 리뷰 반영 / `Codex가 작성`

--- programmers/연습문제/숫자의-표현/main.py
+++ programmers/연습문제/숫자의-표현/main.py
@@ -1,9 +1,9 @@
 def solution(n):
     answer = 1
 
-    for i in range(1, n+1):
+    for i in range(1, n // 2 + 1):
         tmp = i
-        for j in range(i+1, n+1):
+        for j in range(i + 1, n + 1):
             tmp += j
             if tmp >= n:
                 if tmp == n:
