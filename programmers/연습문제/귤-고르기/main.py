9ef0e952e9df01e5590f02e301b934a8c29acc33
han0gu <imgum9@gmail.com>
2026-06-20 13:46:41 +0900

    docs(programmers): 귤 고르기 / 피드백 반영 / `Codex가 작성`

--- programmers/연습문제/귤-고르기/main.py
+++ programmers/연습문제/귤-고르기/main.py
@@ -1,20 +1,15 @@
 from collections import Counter
 
-def solution(k, tang):
-    c = Counter(tang)
-    
-    ls = []
-    for key in c:
-        ls.append((key, c[key]))
-    ls.sort(key=lambda x: -x[1])
-    
-    idx = 0
-    target_cnt = k
-    while True:
-        _, value = ls[idx]
-        
-        if value >= target_cnt:
-            return idx + 1
-        
-        target_cnt -= value
-        idx += 1
+
+def solution(k, tangerine):
+    counts = sorted(Counter(tangerine).values(), reverse=True)
+
+    answer = 0
+    for count in counts:
+        k -= count
+        answer += 1
+
+        if k <= 0:
+            return answer
+
+    return answer
