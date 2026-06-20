e4289e428fe474377777a346ac3d1969ddacc5b1
han0gu <imgum9@gmail.com>
2026-06-20 12:52:36 +0900

    docs(programmers): 짝지어 제거하기 / 공백 정리 / `Codex가 작성`

--- programmers/연습문제/짝지어-제거하기/main.py
+++ programmers/연습문제/짝지어-제거하기/main.py
@@ -1,16 +1,15 @@
 def solution(s):
-    
     stack = []
-    
+
     for c in s:
         if not stack:
             stack.append(c)
             continue
-        
+
         head = stack[-1]
         if head == c:
             stack.pop()
         else:
             stack.append(c)
-            
+
     return 1 if not stack else 0
