4e0baadd2cecd9f7e2e46383f67be68a58cdd05c
han0gu <imgum9@gmail.com>
2026-06-20 14:54:45 +0900

    docs(programmers): 구명보트 / Codex feedback을 반영하여 greedy 풀이 개선
    fix(programmers): 구명보트 포인터 처리 순서 수정 / `Codex가 작성`

--- programmers/greedy/구명보트/main.py
+++ programmers/greedy/구명보트/main.py
@@ -1,24 +1,15 @@
-def solution(people, limit):    
+def solution(people, limit):
     answer = 0
-    
-    people.sort(reverse=True)
-    n = len(people)
-    rescued = [False] * n
-    rescued_cnt = 0
-    
-    while rescued_cnt < n:
+
+    people.sort()
+
+    s = 0
+    e = len(people) - 1
+
+    while s <= e:
+        if people[s] + people[e] <= limit:
+            s += 1
+        e -= 1
         answer += 1
-        boat = 0
-        boat_p_cnt = 0
-        
-        for i in range(n):
-            if not rescued[i] and boat + people[i] <= limit:
-                rescued[i] = True
-                rescued_cnt += 1
-                
-                boat += people[i]
-                boat_p_cnt += 1
-                if boat_p_cnt == 2:
-                    break
-        
+
     return answer
