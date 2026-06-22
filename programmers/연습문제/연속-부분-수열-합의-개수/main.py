5a82a552425f7a2b156e1e33633d0cc087c37236
han0gu <imgum9@gmail.com>
2026-06-22 14:32:17 +0900

    docs(programmers): 연속 부분 수열 합의 개수 / Codex feedback을 반영하여 시간 복잡도 개선

--- programmers/연습문제/연속-부분-수열-합의-개수/main.py
+++ programmers/연습문제/연속-부분-수열-합의-개수/main.py
@@ -1,11 +1,15 @@
 def solution(elements):
-    answer = []
+    answer = set(elements)
     
     n = len(elements)
     new_elements = elements * 2
     
-    for i in range(1, n+1):
-        for j in range(n):
-            answer.append(sum(new_elements[j:j+i]))
+    for i in range(2, n+1):
+        tmp = sum(new_elements[0: i])
+        answer.add(tmp)
+        
+        for j in range(n-1):
+            tmp += new_elements[i+j] - new_elements[j]
+            answer.add(tmp)
     
-    return len(set(answer))
+    return len(answer)
