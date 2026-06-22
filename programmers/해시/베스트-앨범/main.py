ea18026a0140714dd0713b8579bf36fd71f6aa11
han0gu <imgum9@gmail.com>
2026-06-22 15:22:11 +0900

    docs(programmers): 베스트 앨범 / Codex feedback을 반영하여 코드 개선

--- programmers/해시/베스트-앨범/main.py
+++ programmers/해시/베스트-앨범/main.py
@@ -6,18 +6,17 @@ def solution(genres, plays):
     d = defaultdict(lambda: {'total':0, 'songs':[]})
     # print('d', d)
     
-    for i, g in enumerate(genres):
-        d[g]['songs'].append((i, plays[i]))
-        d[g]['total'] += plays[i]
+    for i, genre in enumerate(genres):
+        d[genre]['songs'].append((i, plays[i]))
+        d[genre]['total'] += plays[i]
     # print('d', d)
     
-    
-    sorted_keys = sorted(d, key=lambda x: -d[x]['total'])
+    sorted_genres = sorted(d, key=lambda x: -d[x]['total'])
     # print('sorted_keys', sorted_keys)
     
-    for key in sorted_keys:
-        songs = d[key]['songs']
-        songs.sort(key=lambda x: -x[1])
+    for genre in sorted_genres:
+        songs = d[genre]['songs']
+        songs.sort(key=lambda x: (-x[1], x[0]))
         # print('songs', songs)
         
         for i in range(min(2, len(songs))):
